0846fe298ca5e4f4dee0ca7594601f60872c7a6e
lulu <zhenglulu_0405@163.com>
2025-10-26 20:55:37 +0800

    remove - canonical config

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,10 @@ import Header from "@/components/Header";
 export const metadata: Metadata = {
   title: "顺流而生",
   description: "输入您的盖洛普优势才干排序，AI为您生成定制化的关系、工作、副业成长建议。通过完成任务看着您的成长树茁壮成长！",
-  metadataBase: new URL("https://gallup-product.com"),
-  keywords: ["盖洛普优势", "MBTI性格测试", "个人成长", "职业发展", "AI教练"],
-  alternates: {
-    canonical: './',
-  },
+  // metadataBase: new URL("https://gallup-product.com"),
+  // alternates: {
+  //   canonical: './',
+  // },
 };
 
 export default function RootLayout({
